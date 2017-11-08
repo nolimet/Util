c841f6fe5a036c9402565fa12974f78e6b756158
Jesse Stam <nolimet@stamdata.nl>
2017-11-08 16:45:03 +0100

    Added timer debug script

---
+++ Debug/DebugTimer.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace Util.Debugger
+{
+    public static class DebugTime
+    {
+        static Dictionary<string, DateTime> logtime;
+        public static void Log(string LogKey)
+        {
+            if (logtime == null)
+            {
+                logtime = new Dictionary<string, DateTime>();
+            }
+
+            DateTime currentTime = DateTime.Now;
+            if (logtime.ContainsKey(LogKey))
+            {
+                Debugger.Log(LogKey, "Time Elapsed" + (logtime[LogKey] - currentTime).Milliseconds + "ms");
+                logtime[LogKey] = currentTime;
+            }
+            else
+            {
+                logtime.Add(LogKey, currentTime);
+            }
+        }
+    }
+}
