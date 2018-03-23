3175ab27c3201487067f6457c064a70c6f685726
Jesse Stam <nolimet@stamdata.nl>
2018-03-23 10:51:31 +0100

    Update Common.cs

--- Math/Common.cs
+++ Math/Common.cs
@@ -7,6 +7,7 @@ namespace Utils.Math{
       //linePnt - point the line passes through
       //lineDir - unit vector in direction of line, either direction works
       //pnt - the point to find nearest on line for
+      //Source https://forum.unity.com/threads/how-do-i-find-the-closest-point-on-a-line.340058/
       public static Vector3 NearestPointOnLine(Vector3 linePnt, Vector3 lineDir, Vector3 pnt)
       {
           lineDir.Normalize();//this needs to be a unit vector
