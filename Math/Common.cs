dc9329b588cfbb5e75aaf58d88d79c9a84d02653
Jesse Stam <nolimet@stamdata.nl>
2018-03-23 10:49:05 +0100

    added new mathClass

---
+++ Math/Common.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections
+
+namespace Utils.Math{
+  public partial static class Math{
+  
+      //linePnt - point the line passes through
+      //lineDir - unit vector in direction of line, either direction works
+      //pnt - the point to find nearest on line for
+      public static Vector3 NearestPointOnLine(Vector3 linePnt, Vector3 lineDir, Vector3 pnt)
+      {
+          lineDir.Normalize();//this needs to be a unit vector
+          var v = pnt - linePnt;
+          var d = Vector3.Dot(v, lineDir);
+          return linePnt + lineDir * d;
+      }
+  }
+}
