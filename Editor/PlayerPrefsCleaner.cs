25fee2c37540d53afaa3c8c1ed28869443ebdf78
Jesse Stam <nolimet@stamdata.nl>
2018-08-30 11:27:10 +0200

    added two usefull editor classes

---
+++ Editor/PlayerPrefsCleaner.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class PlayerPrefsCleaner : MonoBehaviour
+{
+    [MenuItem("Util/Clear PlayerPrefs")]
+    static void ClearPlayerPrefs()
+    {
+        PlayerPrefs.DeleteAll();
+        Debug.Log("PlayerPrefs Cleared");
+    }
+
+}
