f0bcafeaaac0c24cce0f5e325e3bd138165840f6
Jesse Stam <nolimet@stamdata.nl>
2020-10-08 19:09:59 +0200

    Reordered scripts around a bit and added asm def files

---
+++ Source/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Jesse D. Stam")]
+[assembly: AssemblyTitle("Jesse Util")]
+[assembly: AssemblyCopyright("Copyright 2020.")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
+[assembly: ComVisible(false)]
