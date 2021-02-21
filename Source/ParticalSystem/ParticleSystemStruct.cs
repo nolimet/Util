27aa482aba9920694173806da5a276200ccfc808
jesse stam <nolimet@stamdata.nl>
2021-02-21 14:03:59 +0100

    added new version of the particle system stuff

---
+++ Source/ParticalSystem/ParticleSystemStruct.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace NoUtil.Particle
+{
+    /// <summary>
+    /// Struct for particle sytems so i don't have to repeat much code
+    /// </summary>
+    [System.Serializable]
+    public struct ParticleSystemStruct
+    {
+        public bool enabled;
+        public ParticleSystem System;
+        public ParticleSystem.Particle[] Particles;
+        public ParticleSystem.EmissionModule Emission;
+        public int maxParticles;
+
+        public void setup()
+        {
+            maxParticles = System.main.maxParticles;
+            if (Particles == null || Particles.Length < maxParticles)
+                Particles = new ParticleSystem.Particle[maxParticles];
+        }
+    }
+}
