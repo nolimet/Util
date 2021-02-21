b130fe2d027d620b3d814c964cd9220c9351b790
jesse stam <nolimet@stamdata.nl>
2021-02-21 14:04:19 +0100

    added UILayoutController

---
+++ Editor/UI/UILayoutControllerEditor.cs
@@ -0,0 +1,173 @@
+﻿using NoUtil.UI;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+
+namespace NoUtil.Editor.UI
+{
+    [CustomEditor(typeof(UILayoutController))]
+    public class UILayoutControllerEditor : UnityEditor.Editor
+    {
+        private const string layoutGroupsName = "layoutGroups";
+        private const string alphaActiveName = "alphaActive";
+        private const string alphaInactiveName = "alphaInactive";
+        private const string defaultLayoutName = "defaultLayout";
+        private const string layoutRootName = "layoutRoot";
+        private const string changeDurationName = "changeDuration";
+
+        private SerializedProperty layoutGroups;
+        private SerializedProperty alphaActive;
+        private SerializedProperty alphaInactive;
+        private SerializedProperty defaultLayout;
+
+        private GUIContent defaultLayoutGUIContent;
+        private GUIContent alphaActiveGUIContent;
+        private GUIContent alphaInactiveGUIContent;
+
+        private ReorderableList layoutGroupsList;
+
+        private void OnEnable()
+        {
+            layoutGroups = serializedObject.FindProperty(layoutGroupsName);
+            alphaActive = serializedObject.FindProperty(alphaActiveName);
+            alphaInactive = serializedObject.FindProperty(alphaInactiveName);
+            defaultLayout = serializedObject.FindProperty(defaultLayoutName);
+
+            layoutGroupsList = new ReorderableList(serializedObject, layoutGroups, true, true, true, true);
+
+            layoutGroupsList.drawElementCallback += OnDrawElement;
+            layoutGroupsList.elementHeightCallback += OnGetElementHeight;
+            layoutGroupsList.onAddCallback += OnElementAdd;
+
+            defaultLayoutGUIContent = new GUIContent(defaultLayout.displayName, defaultLayout.tooltip);
+            alphaActiveGUIContent = new GUIContent("Active", alphaActive.tooltip);
+            alphaInactiveGUIContent = new GUIContent("Inactive", alphaInactive.tooltip);
+        }
+
+        private void OnElementAdd(ReorderableList list)
+        {
+            int length = list.serializedProperty.arraySize++;
+
+            SerializedProperty layoutGroup = layoutGroups.GetArrayElementAtIndex(length);
+            SerializedProperty layoutRoot = layoutGroup.FindPropertyRelative(layoutRootName);
+            SerializedProperty changeDuration = layoutGroup.FindPropertyRelative(changeDurationName);
+
+            layoutRoot.objectReferenceValue = null;
+            changeDuration.floatValue = 0.3f;
+        }
+
+        private float OnGetElementHeight(int index)
+        {
+            return 29;
+        }
+
+        private void OnDrawElement(Rect rect, int index, bool isActive, bool isFocused)
+        {
+            SerializedProperty layoutGroup = layoutGroups.GetArrayElementAtIndex(index);
+
+            SerializedProperty layoutRoot = layoutGroup.FindPropertyRelative(layoutRootName);
+            SerializedProperty changeDuration = layoutGroup.FindPropertyRelative(changeDurationName);
+
+            rect.height = 21;
+            rect.y += 4;
+
+            Rect changeDurationRect = new Rect(rect);
+            changeDurationRect.x += 5f;
+            changeDurationRect.width = 30;
+
+            Rect layoutRootRect = new Rect(rect);
+            layoutRootRect.width -= 30 + 5 + 5;
+            layoutRootRect.x += 30 + 5 + 5;
+
+            EditorGUI.PropertyField(changeDurationRect, changeDuration, GUIContent.none);
+            EditorGUI.PropertyField(layoutRootRect, layoutRoot, GUIContent.none);
+        }
+
+        private void DrawDefaultLayoutSelector()
+        {
+            int length = layoutGroups.arraySize;
+            string[] names = new string[length + 2];
+
+            string currentValue = defaultLayout.stringValue;
+            names[0] = string.IsNullOrWhiteSpace(currentValue) ? "<no default object>" : $"[{currentValue}]";
+            names[1] = "";
+
+            for (int i = 0; i < length; i++)
+            {
+                //get the layoutRootObject reference so we can extract the name from it
+                var obj = layoutGroups.GetArrayElementAtIndex(i).FindPropertyRelative(layoutRootName)?.objectReferenceValue;
+                names[i + 2] = obj ? obj.name : string.Empty;
+            }
+
+            int selectedValue = 0;
+            selectedValue = EditorGUILayout.Popup(defaultLayoutGUIContent, selectedValue, names);
+
+            if (selectedValue > 1)
+            {
+                currentValue = names[selectedValue];
+            }
+
+            defaultLayout.stringValue = currentValue;
+        }
+
+        private void ValidateValues()
+        {
+            //Clamping the alpha values to 0-1 as high values don't work
+            alphaActive.floatValue = Mathf.Clamp01(alphaActive.floatValue);
+            alphaInactive.floatValue = Mathf.Clamp01(alphaInactive.floatValue);
+
+            List<string> usedNames = new List<string>();
+            for (int i = 0; i < layoutGroups.arraySize; i++)
+            {
+                var layoutGroup = layoutGroups.GetArrayElementAtIndex(i);
+                var layoutRoot = layoutGroup.FindPropertyRelative(layoutRootName);
+                var changeDuration = layoutGroup.FindPropertyRelative(changeDurationName);
+
+                changeDuration.floatValue = Mathf.Max(0, changeDuration.floatValue); //making sure there is no negative duration
+
+                var layoutRootRefrenceValue = layoutRoot.objectReferenceValue;
+                usedNames.Add(layoutRootRefrenceValue ? layoutRootRefrenceValue.name : string.Empty);
+            }
+
+            var groupedNames = usedNames.GroupBy(x => x);
+            var duplicateNames = groupedNames.Where(x => x.Count() > 1 && x.Key != string.Empty);
+
+            if (groupedNames.Any(x => x.Key == string.Empty))
+            {
+                EditorGUILayout.HelpBox("There are empty names", MessageType.Warning, true);
+            }
+
+            if (duplicateNames.Count() > 0)
+            {
+                EditorGUILayout.HelpBox($"Duplicate names!\n {string.Join("\n", duplicateNames.Select(x => x.Key))}", MessageType.Error, true);
+            }
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            using (var changeScope = new EditorGUI.ChangeCheckScope())
+            {
+                DrawDefaultLayoutSelector();
+                using (var v1 = new EditorGUILayout.VerticalScope())
+                {
+                    EditorGUILayout.LabelField("Alpha");
+                    using (var h1 = new EditorGUILayout.HorizontalScope())
+                    {
+                        EditorGUILayout.PropertyField(alphaActive, alphaActiveGUIContent);
+                        EditorGUILayout.PropertyField(alphaInactive, alphaInactiveGUIContent);
+                    }
+                }
+                EditorGUILayout.Space();
+                layoutGroupsList.DoLayoutList();
+                ValidateValues();
+                if (changeScope.changed)
+                {
+                    serializedObject.ApplyModifiedProperties();
+                }
+            }
+        }
+    }
+}
---
+++ Source/UI/UILayoutController.cs
@@ -0,0 +1,165 @@
+﻿using NoUtil.Extentsions;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace NoUtil.UI
+{
+    public class UILayoutController : MonoBehaviour
+    {
+        [SerializeField]
+        private float alphaActive = 1, alphaInactive = 0;
+
+        [SerializeField]
+        private string defaultLayout = string.Empty;
+
+        [SerializeField]
+        private LayoutGroup[] layoutGroups;
+
+        private LayoutGroup activeLayoutGroup;
+
+        private CancellationTokenSource tokenSource;
+
+        public void ChangeLayout(GameObject newLayoutRoot)
+        {
+            if (tokenSource != null)
+            {
+                tokenSource.Cancel();
+                tokenSource.Dispose();
+                tokenSource = null;
+            }
+
+            tokenSource = new CancellationTokenSource();
+            if (newLayoutRoot && layoutGroups.Any(x => x.LayoutRoot == newLayoutRoot))
+            {
+                ChangeLayoutAsync(newLayoutRoot, tokenSource.Token).WrapErrors();
+            }
+        }
+
+        public async Task ChangeLayoutAsync(GameObject newLayoutRoot, CancellationToken token)
+        {
+            if (newLayoutRoot && layoutGroups.Any(x => x.LayoutRoot == newLayoutRoot) && activeLayoutGroup.LayoutRoot != newLayoutRoot)
+            {
+                LayoutGroup newLayoutGroup = layoutGroups.First(x => x.LayoutRoot == newLayoutRoot); // find the new layout
+
+                if (newLayoutGroup.ChangeDuration <= 0)
+                {
+                    newLayoutGroup.SetActiveInstant(true, alphaActive);
+                    activeLayoutGroup.SetActiveInstant(false, alphaInactive);
+                }
+                else
+                {
+                    //Create tasks that will run in the until
+                    Task changeTaskNewLayout = newLayoutGroup.ChangeLayoutTask(newLayoutGroup.ChangeDuration, alphaActive, true, token);
+                    Task changeTaskActiveLayout = activeLayoutGroup.ChangeLayoutTask(newLayoutGroup.ChangeDuration, alphaInactive, false, token);
+
+                    //Wait until the tasks are done
+                    await new WaitUntil(WaitUntilTask);
+                    bool WaitUntilTask()
+                    {
+                        return TaskCheck(changeTaskNewLayout) || TaskCheck(changeTaskActiveLayout);
+                    }
+
+                    await new WaitForUpdate();
+                    if (TaskCheck(changeTaskNewLayout))
+                        changeTaskNewLayout.Dispose();
+                    if (TaskCheck(changeTaskActiveLayout))
+                        changeTaskActiveLayout.Dispose();
+                }
+                activeLayoutGroup = newLayoutGroup;
+            }
+
+            bool TaskCheck(Task task) => task.IsCompleted || task.IsFaulted || task.IsCanceled;
+        }
+
+        private void Awake()
+        {
+            layoutGroups = layoutGroups.Where(x => x.LayoutRoot).ToArray(); // getting rid of any empty groups
+            foreach (LayoutGroup layoutGroup in layoutGroups)
+            {
+                layoutGroup.Init();
+                if (layoutGroup.Name == defaultLayout)
+                {
+                    activeLayoutGroup = layoutGroup;
+                    layoutGroup.SetActiveInstant(true, alphaActive);
+                }
+                else
+                {
+                    layoutGroup.SetActiveInstant(false, alphaInactive);
+                }
+            }
+        }
+
+        [System.Serializable]
+        public class LayoutGroup
+        {
+            [SerializeField]
+            private float changeDuration = 0.2f;
+
+            [SerializeField]
+            private GameObject layoutRoot;
+
+            private CanvasGroup canvasGroup;
+
+            public GameObject LayoutRoot => layoutRoot;
+            public string Name => layoutRoot ? layoutRoot.name : string.Empty;
+            public float ChangeDuration => changeDuration;
+
+            public async Task ChangeLayoutTask(float duration, float targetAlpha, bool isEnabled, CancellationToken token)
+            {
+                targetAlpha = Mathf.Clamp01(targetAlpha);
+
+                float maxDelta = (isEnabled ? targetAlpha - canvasGroup.alpha : canvasGroup.alpha - targetAlpha) / duration;
+                try
+                {
+                    if (!layoutRoot.activeSelf)
+                    {
+                        layoutRoot.SetActive(true);
+                    }
+
+                    while (Mathf.Abs(canvasGroup.alpha - targetAlpha) >= float.Epsilon)
+                    {
+                        canvasGroup.alpha = Mathf.MoveTowards(canvasGroup.alpha, targetAlpha, maxDelta * Time.deltaTime);
+                        await new WaitForUpdate();
+                        token.ThrowIfCancellationRequested();
+                    }
+
+                    Debug.Log($"Change completed - {Name}");
+                }
+                catch (OperationCanceledException e)
+                {
+                    Debug.LogWarning(e);
+                }
+                catch (Exception e)
+                {
+                    canvasGroup.alpha = targetAlpha;
+                    Debug.LogException(e);
+                    throw;
+                }
+                finally
+                {
+                    if (!isEnabled)
+                    {
+                        layoutRoot.SetActive(false);
+                    }
+                    canvasGroup.alpha = targetAlpha;
+                }
+            }
+
+            public void SetActiveInstant(bool active, float alpha)
+            {
+                canvasGroup.alpha = alpha;
+                layoutRoot.SetActive(active);
+            }
+
+            public void Init()
+            {
+                canvasGroup = layoutRoot.GetOrAddComponent<CanvasGroup>();
+            }
+
+            public static implicit operator GameObject(LayoutGroup group) => group.LayoutRoot;
+        }
+    }
+}
