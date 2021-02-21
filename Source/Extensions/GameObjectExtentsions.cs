467502ba5113b1a3730e0b839b77163210a03636
jesse stam <nolimet@stamdata.nl>
2021-02-21 14:04:06 +0100

    added gameobject extentions

---
+++ Source/Extensions/GameObjectExtentsions.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NoUtil.Extentsions
+{
+    public static class GameObjectExtentsions
+    {
+        public static T GetOrAddComponent<T>(this Component component) where T : Component
+        {
+            T returnValue = component.GetComponent<T>();
+            if (!returnValue)
+            {
+                returnValue = component.gameObject.AddComponent<T>();
+            }
+            return returnValue;
+        }
+
+        public static T GetOrAddComponent<T>(this GameObject gameObject) where T : Component
+        {
+            T returnValue = gameObject.GetComponent<T>();
+            if (!returnValue)
+            {
+                returnValue = gameObject.AddComponent<T>();
+            }
+            return returnValue;
+        }
+    }
+}
