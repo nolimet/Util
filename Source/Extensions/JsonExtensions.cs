f9c6d90d982752e953485dc36f5ebca5d9eeebc3
Jesse Stam <jesse@jessestam.nl>
2022-08-05 08:39:59 +0200

    Added json extentsions

---
+++ Source/Extensions/JsonExtensions.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using UnityEngine;
+
+namespace NoUtil.Extensions
+{
+    public static class JsonExtensions
+    {
+        public static async Task<bool> ToPathAsync<T>(this T data, string path)
+        {
+            var file = new FileInfo(path);
+            if (file.Directory is { Exists: false })
+            {
+                file.Directory.Create();
+            }
+
+            try
+            {
+                string json = JsonConvert.SerializeObject(data, Formatting.None);
+                using (var writer = file.CreateText())
+                {
+                   await writer.WriteAsync(json);
+                }
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+            }
+
+            return false;
+
+        }
+
+        public static async Task<T> FromPathAsync<T>(this string path)
+        {
+            var file = new FileInfo(path);
+            if (!file.Exists) return default;
+            
+            T result;
+            try
+            {
+                string json;
+                using (var reader = file.OpenText())
+                {
+                    json = await reader.ReadToEndAsync();
+                }
+
+                result = JsonConvert.DeserializeObject<T>(json);
+            }
+            catch (Exception e)
+            {
+                result = default;
+                Debug.LogException(e);
+            }
+
+            return result;
+        }
+
+        public static bool ToPath<T>(this T data, string path)
+        {
+            var file = new FileInfo(path);
+            if (file.Directory is { Exists: false })
+            {
+                file.Directory.Create();
+            }
+
+            try
+            {
+                string json = JsonConvert.SerializeObject(data, Formatting.None);
+                using (var writer = file.CreateText())
+                {
+                    writer.Write(json);
+                }
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+            }
+
+            return false;
+        }
+        
+        public static bool FromPath<T>(this string path, out T data)
+        {
+            data = default;
+            
+            var file = new FileInfo(path);
+            if (!file.Exists) return false;
+            
+            try
+            {
+                string json;
+                using (var reader = file.OpenText())
+                {
+                    json = reader.ReadToEnd();
+                }
+
+                data = JsonConvert.DeserializeObject<T>(json);
+            }
+            catch (Exception e)
+            {
+                data = default;
+                Debug.LogException(e);
+            }
+
+            return data != null;
+        }
+    }
+}
